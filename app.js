9060ce86723396196ab9c7aa575c25e2f3fcb2c9
gnahc2305 <achangmurillo@gmail.com>
2022-09-04 22:39:58 -0500

    Finished without the loop and only works once

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const rndInt = randomIntFromInterval(1, 3)
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+const rndInt = randomIntFromInterval(1, 3);
+
+let c = '';
+let r = 'Rock';
+let p = 'Paper';
+let s = 'Scissors';
 
 function getComputerChoice() {
     let rock = 1;
@@ -7,22 +16,70 @@ function getComputerChoice() {
     let compChoice = rndInt;
     switch (compChoice) {
         case rock:
-            console.log('roca')
+            c = r;
+            return c;
             break;
         case paper:
-            console.log('paper')
+            c = p;
+            return c;
             break
         case scissors:
-            console.log('scissors');
+            c = s;
+            return c;
             break
     }
 }
 
-getComputerChoice()
+// let playerSelectionCaseSensitive = 'scissors';
+// let playerSelection = playerSelectionCaseSensitive.toLocaleLowerCase();
+
+let playerSelection = prompt('Your choice: ').toLowerCase();
 
 
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
- 
+console.log('Player: ' + playerSelection)
+console.log('Computer: ' + getComputerChoice())
+
+
+
+
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'Rock') {
+        console.log('Tie! Try Again');
+    } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+        console.log('You lose! Paper beats rock');
+    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        console.log('You win! Rock beats scissors');
+    }
+
+    else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        console.log('You Win! Paper beats rock');
+    } else if (playerSelection === 'paper' && computerSelection === 'Paper') {
+        console.log('Tie! Try Again');
+    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        console.log('You lose! Scissors beats paper');
+    }
+
+    else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        console.log('You lose! Rock beats scissors');
+    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        console.log('You win! Scissors beats paper');
+    } else if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
+        console.log('Tie! Try Again');
+    }
+}
+
+playRound(playerSelection, computerSelection);
+
+
+
+// let playerSelection = prompt('Select your choice: ').toLowerCase();
+
+// function game(playerSelection, computerSelection) {
+//     for (let i = 0; i < 5; i++) {
+
+//     }
+// }
+
+// game(playerSelection, computerSelection);
